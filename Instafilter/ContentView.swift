2e7f9de8da5b80f7bd6443d083aa62c0ebc4f7c7
JulyKikuAkita <b92701105@gmail.com>
2021-12-07 22:01:49 -0800

    Initial Commit

---
+++ Instafilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Ifang Lee on 12/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Ifang Lee on 12/7/21.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
